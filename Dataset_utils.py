9a75ed7e0b07a6ba36c73c44d199e8dd1b951bc6
VVk-dev <varunvenkwrk@gmail.com>
2024-03-05 16:14:42 -0500

    Renamed insert_vectors_of_data() to insert_vectors_from_data() and modified it to not rely on a global variable

--- Dataset_utils.py
+++ Dataset_utils.py
@@ -1,8 +1,6 @@
 from OpenAI_utils import get_embedding
 from Pinecone_utils import insert_vector_into_pinecone_index
 
-global filetext
-
 #Split file into chunks and load into filetext
 
 def get_data_from_file(filepath: str) -> list[str]:
@@ -19,13 +17,13 @@ def get_data_from_file(filepath: str) -> list[str]:
 
 #Get and insert vector for each chunk into pinecone index
 
-def insert_vectors_of_data():        
+def insert_vectors_from_data(filetext : list[str]):        
     
     for i in range(0, len(filetext)):
         
         vector_val = get_embedding(filetext[i])
         
-        #use id and vector_val to create entry into vector index in proper format
+        #use index of chunk as id and its vector as vector_val to create entry into vector index in proper format
         
         vector = {"id" : str(i), "values" : vector_val}
         
