5d8c58e30372e04ec08899aeae5591befa42f210
VVk-dev <varunvenkwrk@gmail.com>
2024-03-05 16:15:58 -0500

    Added first step of getting chunks from data file

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv, find_dotenv
 import Llama2_utils
 import OpenAI_utils
@@ -6,4 +7,8 @@ import Dataset_utils
 
 #Initailize global variables
 _ = load_dotenv(find_dotenv())
+
+#Step 1: Get chunks from file
+
+filechunks : list[str] = Dataset_utils.get_data_from_file(os.getenv("DATA_FILE_PATH"))
 
