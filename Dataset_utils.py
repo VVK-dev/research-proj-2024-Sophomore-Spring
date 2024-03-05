bc7449381dc4515349a5342564a77cd9a95ff4c0
VVk-dev <varunvenkwrk@gmail.com>
2024-03-05 14:52:16 -0500

    Added function to insert vectors of chunks into the pinecone index

--- Dataset_utils.py
+++ Dataset_utils.py
@@ -14,3 +14,17 @@ def get_data_from_file(filepath: str):
         
         #the filetext variable now contains a list of chunks
 
+
+#Get and insert vector for each chunk into pinecone index
+
+def insert_vectors_of_data():        
+    
+    for i in range(0, len(filetext)):
+        
+        vector_val = get_embedding(filetext[i])
+        
+        #use id and vector_val to create entry into vector index in proper format
+        
+        vector = {"id" : str(i), "values" : vector_val}
+        
+        insert_vector_into_pinecone_index(vector)
