e59b7bfef961488a37888fb3711cda8110a1ce6a
VVk-dev <varunvenkwrk@gmail.com>
2024-03-05 16:18:27 -0500

    Added fourth step in RAG process of getting context for the prompt

--- Main.py
+++ Main.py
@@ -34,3 +34,27 @@ prompt5 : str = "<FIFTH PROMPT>"
 
 prompts_with_context : dict[str,str] = {prompt1 : None, prompt2 : None, prompt3 : None, prompt4 : None, prompt5: None}
 
+#Step 4: Get context for prompt 
+
+for prompt in prompts_with_context.keys():
+    
+    #Sub-step 1 - get vector embedding of prompt 
+    
+    vector : list[float] = OpenAI_utils.get_embedding(prompt)
+    
+    #Sub-step 2 - query over index
+    
+    matching_ids : list[str] = Pinecone_utils.query_pinecone_index(vector)
+    
+    #Sub-step 3 - join matching chunks together to get context as a single string
+    
+    context : str = ""
+    
+    for id in matching_ids:
+        
+        context += (filechunks[int(id)])
+    
+    #Sub-step 4 - update prompt with context
+    
+    prompts_with_context.update({prompt : context})
+
