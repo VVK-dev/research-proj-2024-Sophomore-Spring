7080e75f9de727729c55999513ebcd89c82aeab6
VVk-dev <varunvenkwrk@gmail.com>
2024-03-24 20:40:31 -0400

    Added method to create neo4j vector index

--- Neo4j_utils.py
+++ Neo4j_utils.py
@@ -1,23 +1,23 @@
 import os
 from langchain_community.vectorstores.neo4j_vector import Neo4jVector
 from langchain_community.graphs import Neo4jGraph
-from langchain_openai import OpenAIEmbeddings
-from langchain_core import documents
-from neo4j import Driver, GraphDatabase, EagerResult
-
-URI = os.getenv("NEO4J_URI")
-Username, Password = os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD")
-
-vector_index : Neo4jVector = Neo4jVector(url = URI, username = Username, password = Password, database = "neo4j").from_existing_index(index_name = "")
-
-driver : Driver = GraphDatabase.driver(URI, auth = (Username, Password))
-
-def check_connectivity():
+#Create vector index if it doesn't already exist
+def create_neo4j_vector_index(knowledge_graph : Neo4jGraph):
     
-    driver.verify_connectivity()
-
-def get_similar_nodes_from_neo4j_vector_index(prompt : str):
+    #excecute a cypher query to create a vector index suited for OpenAI's text-embedding-3-small
     
+    knowledge_graph.query("""
+    CREATE VECTOR INDEX entity_embeddings IF NOT EXISTS
+    FOR (n:Entity) ON (n.nameEmbedding) 
+    OPTIONS { indexConfig: {
+        `vector.dimensions`: 1536,
+        `vector.similarity_function`: 'cosine'
+    }}"""
+    )
+
+    #All nodes have the 'Entity' label, so this vector index will be for all nodes in the graph
+
+
 #Search vector index to get top 2 nodes related to prompt and all of their relationships and neighbors 
 def search_neo4j_vector_index(knowledge_graph : Neo4jGraph, OpenAIKey : str, prompt : str) -> str:
     
