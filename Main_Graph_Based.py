86ec19f797f6324ad8c0739e7a057f68f87256d6
VVk-dev <varunvenkwrk@gmail.com>
2024-03-30 17:14:31 -0400

    Added step 2: populate vector index and step 3: get prompts

--- Main_Graph_Based.py
+++ Main_Graph_Based.py
@@ -23,3 +23,18 @@ knowledge_graph = Neo4jGraph(url = Neo4j_URI, username = Username, password = Pa
 #Step 1: Create vector index if it doesn't already exist
 
 Neo4j_utils.create_neo4j_vector_index(knowledge_graph)
+
+#Step 2: Populate vector index if empty
+
+Neo4j_utils.populate_neo4j_vector_index(knowledge_graph = knowledge_graph, OpenAIKey = OpenAIKey)
+
+#Step 3: Get prompts
+
+prompt1 : str = "Tell me about <SOMETHING>." #Overall descriptive question
+prompt2 : str = "<SECOND PROMPT>"
+prompt3 : str = "<THIRD PROMPT>"
+prompt4 : str = "Ask me a question about <SOMETHING>." #Checking the model's understanding of the text
+prompt5 : str = "Ask me a question about <SOMETHING> and <SOMETHING>." #Checking the model's understanding of multiple texts together
+
+prompts_with_context : dict[str,str] = {prompt1 : None, prompt2 : None, prompt3 : None, prompt4 : None, prompt5: None}
+
