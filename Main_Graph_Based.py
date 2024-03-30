6664496afd45dd62ecad92a22922fa6bfa19b608
VVk-dev <varunvenkwrk@gmail.com>
2024-03-30 17:13:55 -0400

    Added loading of graph and Step 1: creation of vector index in neo4j

--- Main_Graph_Based.py
+++ Main_Graph_Based.py
@@ -15,3 +15,11 @@ _ = load_dotenv(find_dotenv(filename = "Keys.env"))
 Neo4j_URI = os.getenv("NEO4J_URI")
 Username, Password = os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD")
 OpenAIKey = os.getenv("OPENAI_API_KEY")
+
+#Load Graph
+
+knowledge_graph = Neo4jGraph(url = Neo4j_URI, username = Username, password = Password, database = "neo4j")
+
+#Step 1: Create vector index if it doesn't already exist
+
+Neo4j_utils.create_neo4j_vector_index(knowledge_graph)
