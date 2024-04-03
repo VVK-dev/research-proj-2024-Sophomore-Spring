46187f59e503d6f6b6f35b9445c7fa1315b09af8
VVk-dev <varunvenkwrk@gmail.com>
2024-04-03 14:58:17 -0400

    Added 20 prompts, 10 description based and 10 relation based

--- Main_Graph_Based.py
+++ Main_Graph_Based.py
@@ -32,13 +32,38 @@ Neo4j_utils.populate_neo4j_vector_index(knowledge_graph = knowledge_graph, OpenA
 
 #Step 3: Get prompts
 
-prompt1 : str = "Tell me about <SOMETHING>." #Overall descriptive question
-prompt2 : str = "<SECOND PROMPT>"
-prompt3 : str = "<THIRD PROMPT>"
-prompt4 : str = "Ask me a question about <SOMETHING>." #Checking the model's understanding of the text
-prompt5 : str = "Ask me a question about <SOMETHING> and <SOMETHING>." #Checking the model's understanding of multiple texts together
+#1st set of prompts - Overall descriptive questions
 
-prompts_with_context : dict[str,str] = {prompt1 : None, prompt2 : None, prompt3 : None, prompt4 : None, prompt5: None}
+descibe_prompt1 : str = "Tell me about Édouard Manet." 
+descibe_prompt2 : str = "Tell me about the 2004 Indian Ocean earthquake."
+descibe_prompt3 : str = "Tell me about the 19th century."
+descibe_prompt4 : str = "Tell me about the 16th century."
+descibe_prompt5 : str = "Tell me about the 1973 Oil Crisis."
+descibe_prompt6 : str = "Tell me about the 2004 Atlantic hurricane season."
+descibe_prompt7 : str = "Tell me about the 2005 Atlantic hurricane season."
+descibe_prompt8 : str = "Tell me about the 1st century."
+descibe_prompt9 : str = "Tell me about the 1980 eruption of Mount St. Helens."
+descibe_prompt10 : str = "Tell me about 1st century BC."
+
+#2nd set of prompts - Checking the model's understanding of how multiple texts relate
+relation_prompt1 : str = "How did Édouard Manet influence Europe?"
+relation_prompt2 : str = "What type of paintings would Édouard Manet paint?"
+relation_prompt3 : str = "Were there any relationships between the 2005 Indian Ocean earthquake and the South China Sea?"
+relation_prompt4 : str = "How did the 1980 eruption of Mount St. Helens affect tourism?"
+relation_prompt5 : str = "What happened to the Ottoman Empire in the 19th century?"
+relation_prompt6 : str = "What was the 2005 Hertfordshire Oil Storage Terminal Fire adjacent to?"
+relation_prompt7 : str = "What led to the Yom Kippur War?"
+relation_prompt8 : str = "What major war preceded the 1973 oil crisis?"
+relation_prompt9 : str = "What hurricane season did hurricane Epsilon belong to?"
+relation_prompt10 : str = "Which countries did the 2005 Atlanic hurricane season affect?"
+
+prompts_with_context : dict[str,str] = {
+    descibe_prompt1 : None, descibe_prompt2 : None, descibe_prompt3 : None, descibe_prompt4 : None,
+    descibe_prompt5: None, descibe_prompt6 : None, descibe_prompt7 : None, descibe_prompt8 : None, 
+    descibe_prompt9 : None, descibe_prompt10 : None, relation_prompt1 : None, relation_prompt2 : None,
+    relation_prompt3 : None, relation_prompt4 : None, relation_prompt5 : None, relation_prompt6 : None,
+    relation_prompt7 : None, relation_prompt8 : None, relation_prompt9 : None, relation_prompt10 : None    
+    }
 
 #Step 4: Get context for prompts
 
