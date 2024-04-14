c9688d41b7cf4433ce7948754b3dd1e27440560d
VVk-dev <varunvenkwrk@gmail.com>
2024-04-14 19:50:23 -0400

    Modified token chopper to work for gpt 3.5 T and gpt 4 T P

--- Tests 3 and 4 - Barbie RAG/Test 3 - GPT 4 T P Relationships/Dataset_utils.py
+++ Tests 3 and 4 - Barbie RAG/Test 3 - GPT 4 T P Relationships/Dataset_utils.py
@@ -8,9 +8,15 @@ def get_paragraphs_from_file(article_path: str) -> list[str]:
         return file.read().split("\n\n")
 
 
-def token_chopper(context : str):
+def token_chopper(context : str, model : str = "gpt-4-turbo-preview"):
     
-    while(OpenAI_utils.num_tokens_from_string(context) >= 100000): #128000 is max token limit for gpt 4 turbo (prompt + completion) 
+    limit :int = 120000 #128000 is max token limit for gpt 4 turbo (prompt + completion) 
+    
+    if(model == "gpt-3.5-turbo"):
+        
+        limit = 12000 #16000 is max token limit for gpt 3.5 turbo (prompt + completion) 
+    
+    while(OpenAI_utils.num_tokens_from_string(context) >= limit):
         
         # Split the paragraph into lines
         
