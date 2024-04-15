54b2e7bc953ddd5df4db9305e83d2aaecc0baf12
VVk-dev <varunvenkwrk@gmail.com>
2024-04-14 20:06:19 -0400

    Modified get_nodes_and_relationships_from_chunk() to get_response() and so that it can be used to answer questions rather than only get relationships

--- Tests 3 and 4 - Barbie RAG/Test 3 - GPT 4 T P Relationships/OpenAI_utils.py
+++ Tests 3 and 4 - Barbie RAG/Test 3 - GPT 4 T P Relationships/OpenAI_utils.py
@@ -29,52 +29,64 @@ def get_embedding(input_str: str) -> list[float]:
     return response.data[0].embedding
 
 #Prompt ChatGPT for nodes and relationships    
-def get_nodes_and_relationships_from_chunk(prompt, model="gpt-4-turbo-preview", temperature=0) -> str: #NOTE: TEST 3 WITH GPT3.5 THEN TEST 3.5 WITH GPT4 TURBO PREVIEW
+def get_response(prompt, model="gpt-4-turbo-preview", is_question : bool = False) -> str:
     
-    system_message :str = """You are an assistant helping create a knowledge graph about a movie in neo4j.
-    Given a paragraph about the movie, you will state all the possible nodes, a label for each node and all relationships possible between each node.
-    Each node can only have 1 label.
-    Each label must be at least 1 word and at most 5 words.
-    You can only give one relationship per line in your responses. 
-    Each relationship must be at least one word and at most 10 words.
-    Relationships cannot start with numbers.
+    system_message :str = ""
     
-    Try to give as many relationships as you can in your response. 
-    You must capture every single possible bit of information from the paragraph in the nodes and relationships you give.
-     
-    Do not repeat relationships that are the same or similar.
-    You must use underscores in place of spaces in your responses.
-    Do not use any special characters other than underscores in your responses.
+    if(is_question):
+        
+        system_message = """You are an AI assistant that responds to questions about the 2023 live action Barbie
+        movie using context provided about the movie that relates to whatever prompts you recieve."""
     
-    If you cannot form any relationships, or if all the relationships you can form are already present in the graph, respond with NONE.
+    else:
+        
+        #if is not question, then the model is being used to create a knowledge graph
+        
+        system_message = """You are an assistant helping create a knowledge graph about a movie in neo4j.
+        Given a paragraph about the movie, you will state all the possible nodes, a label for each node and all relationships possible between each node.
+        Each node can only have 1 label.
+        Each label must be at least 1 word and at most 5 words.
+        You can only give one relationship per line in your responses. 
+        Each relationship must be at least one word and at most 10 words.
+        Relationships cannot start with numbers.
+        
+        Try to give as many relationships as you can in your response. 
+        You must capture every single possible bit of information from the paragraph in the nodes and relationships you give.
+        
+        Do not repeat relationships that are the same or similar.
+        You must use underscores in place of spaces in your responses.
+        Do not use any special characters other than underscores in your responses.
+        
+        If you cannot form any relationships, or if all the relationships you can form are already present in the graph, respond with NONE.
+        
+        Do not number your responses.
+        
+        Each line in your response should follow the following format:
+        
+        NodeName(NodeLabel)->Relationship->NodeName(NodeLabel)
+        
+        Example:
+        
+        prompt = 
+        
+        Barbie is a 2023 fantasy comedy film directed by Greta Gerwig from a screenplay she wrote with Noah Baumbach.
+        
+        your response = 
+        
+        Barbie(Movie)->is_genre->Fantasy(Genre) 
+        Barbie(Movie)->is_genre->Comedy(Genre)
+        Barbie(Movie)->released_in->2023(Year)
+        Barbie(Movie)->directed_by->Greta_Gerwig(Director)
+        Barbie(Movie)->screenplay_written_by->Greta_Gerwig(Director)
+        Barbie(Movie)->screenplay_written_by->Noah_Baumbach(Screenwriter)
+        Greta_Gerwig(Director)->wrote_screenplay_for_Barbie_with->Noah_Baumbach(Screenwriter)
+        """
     
-    Do not number your responses.
-    
-    Each line in your response should follow the following format:
-    
-    NodeName(NodeLabel)->Relationship->NodeName(NodeLabel)
-    
-    Example:
-    
-    prompt = 
-    
-    Barbie is a 2023 fantasy comedy film directed by Greta Gerwig from a screenplay she wrote with Noah Baumbach.
-    
-    your response = 
-    
-    Barbie(Movie)->is_genre->Fantasy(Genre) 
-    Barbie(Movie)->is_genre->Comedy(Genre)
-    Barbie(Movie)->released_in->2023(Year)
-    Barbie(Movie)->directed_by->Greta_Gerwig(Director)
-    Barbie(Movie)->screenplay_written_by->Greta_Gerwig(Director)
-    Barbie(Movie)->screenplay_written_by->Noah_Baumbach(Screenwriter)
-    Greta_Gerwig(Director)->wrote_screenplay_for_Barbie_with->Noah_Baumbach(Screenwriter)
-    """
     #reduce size of prompt if too big. This will only happen when there are too many relationships in graph 
     #given as part of the prompt. Here, token chopper will only remove excess relationships rather than
     #parts of the article chatgpt needs to create relationships for.
     
-    prompt = token_chopper(prompt) 
+    prompt = token_chopper(context = prompt, model = model) 
     
     messages = [
     {'role': 'system', 'content': system_message},
@@ -84,6 +96,6 @@ def get_nodes_and_relationships_from_chunk(prompt, model="gpt-4-turbo-preview", 
     response = client.chat.completions.create(
         model=model,
         messages=messages,
-        temperature=temperature 
+        temperature=0 
     )
     return response.choices[0].message.content
